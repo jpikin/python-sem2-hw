860b08b0aa5530a8f8a42b60ca5075018c6b5792
j.pikin <pikin@bk.ru>
2023-06-21 14:54:50 +0500

    task 10 2 var added

--- task10.py
+++ task10.py
@@ -5,9 +5,25 @@
 
 from random import randint
 
+print("Вариант 1:")
 money = [randint(0,1) for _ in range(int(input("Введите количество монет ")))]
 avers_revers = [0,0]
 for i in money:
     if i == 0: avers_revers[0] += 1
     else: avers_revers[-1] += 1
+print(*money)
 print(f'Минимальное количество монет, которые нужно перевернуть: {min(avers_revers)}') 
+
+
+#####################################################
+# 2 ВАРИАНТ
+# from random import randint
+print("Вариант 2:")
+avers_revers = [0,0]
+for _ in range(int(input("Введите количество монет "))):
+    x = randint(0,1)
+    if x == 0: avers_revers[0] += 1
+    else: avers_revers[-1] += 1
+    print(x, end=' ')
+print(f': Минимальное количество монет, которые нужно перевернуть: {min(avers_revers)}')     
+
